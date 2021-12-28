f367ab82a436f96c4780c4d2f0bf95c37f9098d1
Deepak Awasthi <awasthi12deepak@gmail.com>
2021-12-28 22:35:50 +0530

    Final Commit!

--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = "You are awesome and innocent!";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty Likaeable!";
+    } else if (resultScore <= 16) {
+      resultText = " You're --- Strange";
+    } else {
+      resultText = "You're Baddd!";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('You Did It!'));
+    return Center(
+      child: Text(resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+    );
   }
 }
